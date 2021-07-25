9c7e36ba59177be8ed45799291cd3319f3ea48c6
전은형 <ooooo5h91@gmail.com>
2021-07-25 18:57:42 +0900

    실제 방 데이터를 목록에 추가

--- app/src/main/java/com/eunhyung/a20210725_jickbangcopy/MainActivity.kt
+++ app/src/main/java/com/eunhyung/a20210725_jickbangcopy/MainActivity.kt
@@ -2,10 +2,26 @@ package com.eunhyung.a20210725_jickbangcopy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eunhyung.a20210725_jickbangcopy.datas.Room
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mRooms = ArrayList<Room>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRooms.add( Room(7500, "서울시 동대문구", 2, "동대문구의  방입니다."))
+        mRooms.add( Room(14500, "서울시 동대문구", 5, "동대문구의 방입니다."))
+        mRooms.add( Room(95000, "서울시 서대문구", 0, "동대문구의 방입니다.."))
+        mRooms.add( Room(3500, "서울시 중랑구", -3, "중랑구의  방입니다."))
+        mRooms.add( Room(9000, "서울시 송파구", 20, "동대문구의 방입니다."))
+        mRooms.add( Room(100000, "서울시 강남구", -2, "강남구의 방입니다."))
+        mRooms.add( Room(680000, "서울시 서초구", -1, "동대문구의 방입니다."))
+        mRooms.add( Room(8500, "서울시 중구", 15, "중구의 방입니다."))
+        mRooms.add( Room(3400, "서울시 중구", 0, "중구의 방입니다."))
+        mRooms.add( Room(156000, "서울시 동대문구", 6, "동대문구의 방입니다."))
     }
 }
